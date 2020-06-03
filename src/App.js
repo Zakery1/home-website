ab6fb8a541493591300b67435f4bd336559e9b2e
Zachary Graham <zakgraham@gmail.com>
2020-06-02 23:01:51 -0700

    add resume pdf link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './document/special.css';
+import pdf from './document/resume.pdf';
 
 function App() {
   return (
     <div className="App">
-      Zachary Graham
+      <h1 className="special">Zachary Graham</h1>
+      <a href={pdf}>View Resume</a>
 
     </div>
   );
