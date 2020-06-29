f56b87a0b7e793676368770ed5ad4d41677f2236
Zachary Graham <zakgraham@gmail.com>
2020-06-28 20:52:42 -0700

    fix accessibility issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 
-import pdf from './document/resume.pdf';
-
-
 function App() {
   return (
     <div className="App">
       <h1 className="special">Zachary Graham</h1>
-      <a aria-required="true" href={pdf} target="_blank" rel="noopener noreferrer">
+      <a href="./document/resume.pdf" target="_blank" rel="noopener noreferrer">
         <img className="pdf-logo" src={ require('./asset/pdf-logo.png') } alt="pdf logo"/>
         <br/>
       </a>
-      <a >Resume</a>
+      <label>Resume</label>
     </div>
   );
 }
