a4cc1fb487d13c999d73219688cf3c464cc1994d
Zachary Graham <zakgraham@gmail.com>
2020-08-15 19:00:54 -0700

    update font color

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
-import './Home.scss';
+import "./Home.scss";
 
 function Home() {
   return (
     <header className="zg-home-container">
-      <img className="zg-headshot" src={require("../../asset/zachary-headshot.png")} alt="zg-headshot"></img>
-      <p className="zg-home-description">Hi, I'm Zak.</p>
+      <img
+        className="zg-headshot"
+        src={require("../../asset/zachary-headshot.png")}
+        alt="zg-headshot"
+      ></img>
+      <p className="zg-home-description">
+        I'm Zak. I'm a web developer, credited <br /> photographer and movie
+        podcast host.
+      </p>
+      <br />
+      <div className="zg-link-holder">
+        <a className="zg-link" href="github.com/zakery1">
+          Github
+        </a>
+        <a className="zg-link" href="https://www.linkedin.com/in/zakgraham/">
+          LinkedIn
+        </a>
+      </div>
     </header>
   );
 }
