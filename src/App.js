d2518f11a1cf21c2bc3d060691f0b3b0eff36a57
Zachary Graham <zakgraham@gmail.com>
2020-08-15 17:55:40 -0700

    remove sidebar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./App.scss";
 
-import Photography from './component/Photography/Photography';
-import Podcast from './component/Podcast/Podcast';
+import Photography from "./component/Photography/Photography";
+import Podcast from "./component/Podcast/Podcast";
 import Calendar from "./component/Calendar/Calendar";
-import Resume from './component/Resume/Resume';
-import Home from './component/Home/Home';
-import Sidebar from "./component/Sidebar/Sidebar";
-
-import routes from "./routes";
+import Resume from "./component/Resume/Resume";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <div className="zg-routes">{routes()}</div>
       <Home />
       <Resume />
       <Podcast />
