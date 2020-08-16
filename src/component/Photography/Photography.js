72b90435273d46fe20e300ae2ff40ec70174c99c
Zachary Graham <zakgraham@gmail.com>
2020-08-15 18:23:47 -0700

    add alt to image

--- src/component/Photography/Photography.js
+++ src/component/Photography/Photography.js
@@ -26,7 +26,7 @@ export default function Photography() {
   const photos = photoArray.map((photo) => {
     return (
       <div key={photo.toString()} className="zg-photos">
-        <img className="zg-photo" src={photo} />
+        <img className="zg-photo" src={photo} alt="Zak"/>
       </div>
     );
   });
