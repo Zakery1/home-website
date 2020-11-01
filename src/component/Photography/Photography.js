2b08e40a4e8ca794253e986db8002db3c55cbfb1
Zachary Graham <zakgraham@gmail.com>
2020-10-31 22:21:09 -0700

    add photography carousel

--- src/component/Photography/Photography.js
+++ src/component/Photography/Photography.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import { Carousel } from "react-responsive-carousel";
+
 import {
   BigClouds,
   Camelback,
@@ -26,13 +31,16 @@ export default function Photography() {
   const photos = photoArray.map((photo) => {
     return (
       <div key={photo.toString()} className="zg-photos">
-        <img className="zg-photo" src={photo} alt="Zak"/>
+        <img className="zg-photo" src={photo} alt="Zak" />
       </div>
     );
   });
 
-  return <div className="zg-photography-container">
-    <h2 className="zg-photography-header">Photography</h2>
-    {photos}
-    </div>;
+  return (
+    <div className="zg-photography-container">
+      <h2 className="zg-photography-header">Photography</h2>
+      <div className="zg-carousel-container"></div>
+      <Carousel>{photos}</Carousel>
+    </div>
+  );
 }
